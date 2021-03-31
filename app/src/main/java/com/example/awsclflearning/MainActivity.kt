d80f1f158bf699122e863cd0f37744a174b7aef3
YutaroM <yutaroapp@gmail.com>
2021-04-01 08:28:17 +0900

    ・トップ画面の基本構成 ・Toolbarの実装 ・dimensの追加と定義

--- app/src/main/java/com/example/awsclflearning/MainActivity.kt
+++ app/src/main/java/com/example/awsclflearning/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.awsclflearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.awsclflearning.R.id.toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolBar = findViewById<Toolbar>(toolbar)
+        setSupportActionBar(toolBar)
     }
 }
