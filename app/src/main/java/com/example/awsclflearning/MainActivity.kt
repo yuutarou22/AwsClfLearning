0c2d39cc051c4820935ac3bbd52f7aa9e57a1b05
YutaroM <yutaroapp@gmail.com>
2021-04-01 21:44:51 +0900

    ・トップ画面の画面遷移実装 ・リファクタリング

--- app/src/main/java/com/example/awsclflearning/MainActivity.kt
+++ app/src/main/java/com/example/awsclflearning/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.awsclflearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import com.example.awsclflearning.R.id.toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolBar = findViewById<Toolbar>(toolbar)
-        setSupportActionBar(toolBar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitle(R.string.app_name)
+
+        aws_practice_button.setOnClickListener {
+            var intent = Intent(this, PracticeActivity::class.java)
+            startActivity(intent)
+        }
+
+        aws_learning_button.setOnClickListener {
+            var intent = Intent(this, LearningActivity::class.java)
+            startActivity(intent)
+        }
+
+        settings_button.setOnClickListener {
+            var intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
