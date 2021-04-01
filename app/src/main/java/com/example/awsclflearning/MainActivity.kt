05f31d961fa9535c3f6f4a43e8765920801e30f0
YutaroM <yutaroapp@gmail.com>
2021-04-01 21:49:40 +0900

    AdMobの実装（テスト広告コードで仮実装）

--- app/src/main/java/com/example/awsclflearning/MainActivity.kt
+++ app/src/main/java/com/example/awsclflearning/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.awsclflearning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import com.example.awsclflearning.R.id.toolbar
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolBar = findViewById<Toolbar>(toolbar)
-        setSupportActionBar(toolBar)
+        setSupportActionBar(toolbar)
+        toolbar.setTitle(R.string.app_name)
+
+        MobileAds.initialize(this){}
+        val adRequest = AdRequest.Builder().build()
+        ad_area.loadAd(adRequest)
+
+        aws_practice_button.setOnClickListener {
+            var intent = Intent(this, PracticeActivity::class.java)
+            startActivity(intent)
+        }
+
+        aws_learning_button.setOnClickListener {
+            var intent = Intent(this, LearningActivity::class.java)
+            startActivity(intent)
+        }
+
+        settings_button.setOnClickListener {
+            var intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
