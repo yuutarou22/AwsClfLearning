7c9e871bb08776a757a8cfb009d6303b114f5383
YutaroM <yutaroapp@gmail.com>
2021-04-04 20:37:19 +0900

    ・折り畳み可能なListView（ExpandableListView）の実装 ・トップ画面のレイアウト修正

---
+++ app/src/main/java/com/example/awsclflearning/Adapter/LearningContentAdapter.kt
@@ -0,0 +1,78 @@
+package com.example.awsclflearning.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import com.example.awsclflearning.R
+
+class LearningContentAdapter(context: Context, _listGroup: List<String>, _listItem: List<List<String>>):
+    BaseExpandableListAdapter() {
+    /** メンバ変数 **/
+    var listGroup: List<String> // 親要素のリスト
+    var listItem: List<List<String>> // 子要素のリスト
+    var context: Context
+
+    override fun getGroupCount(): Int {
+        return listGroup.size
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return listItem[groupPosition].size
+    }
+
+    override fun getGroup(groupPosition: Int): Any {
+        return listGroup[groupPosition]
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        return listItem[groupPosition][childPosition]
+    }
+
+    override fun getGroupId(p0: Int): Long {
+        return 0
+    }
+
+    override fun getChildId(p0: Int, p1: Int): Long {
+        return 0
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        if (convertView == null) {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.list_group, parent, false)
+        }
+
+        val textView = convertView!!.findViewById<TextView>(R.id.list_group_title)
+        textView.text = listGroup[groupPosition]
+        return convertView
+    }
+
+    override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var convertView = convertView
+        if(convertView == null) {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.list_item, parent, false)
+        }
+        val textView = convertView!!.findViewById<TextView>(R.id.list_item_contents)
+        textView.text = listItem[groupPosition][childPosition]
+        return convertView
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        return true
+    }
+
+    init {
+        this.context = context
+        this.listGroup = _listGroup
+        this.listItem = _listItem
+    }
+}
--- app/src/main/java/com/example/awsclflearning/Fragment/LearningFragment.kt
+++ app/src/main/java/com/example/awsclflearning/Fragment/LearningFragment.kt
@@ -5,12 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.awsclflearning.Adapter.LearningContentAdapter
 import com.example.awsclflearning.R
+import kotlinx.android.synthetic.main.fragment_learning.*
 
 class LearningFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 親グループの作成
+        val group: MutableList<String> = ArrayList()
+        group.add("基礎")
+        group.add("AWSサービス")
+        group.add("オススメ書籍")
+
+        // 子グループの作成
+        val item_group1: MutableList<String> = ArrayList()
+        item_group1.add("Group1ーA")
+        item_group1.add("Group2ーB")
+
+        val item_group2: MutableList<String> = ArrayList()
+        item_group2.add("Group2ーA")
+        item_group2.add("Group2ーB")
+        item_group2.add("Group2ーC")
+        item_group2.add("Group2ーD")
+
+        val item_group3: MutableList<String> = ArrayList()
+        item_group3.add("Group3ーA")
+        item_group3.add("Group3ーB")
+        item_group3.add("Group3ーC")
+        item_group3.add("Group3ーD")
+
+        val items: MutableList<List<String>> = ArrayList()
+        items.add(item_group1)
+        items.add(item_group2)
+        items.add(item_group3)
+
+        // ExpandableListViewの初期化
+        val adapter = context?.let { LearningContentAdapter(it, group, items) }
+        expandable_list_view.setAdapter(adapter)
+
+        // 子要素をタップしたときの処理
+        expandable_list_view.setOnChildClickListener { parent, v, groupPosition, childPosition, id ->
+            val adapter1 = parent.expandableListAdapter as LearningContentAdapter
+            val groupName = adapter1.getGroup(groupPosition) as String
+            val itemName = adapter1.getChild(groupPosition, childPosition) as String
+            Toast.makeText(context, "$groupName : $itemName", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
