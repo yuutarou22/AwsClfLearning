190033a15a2b460754dd878b1df836d1e257f8a0
YutaroM <yutaroapp@gmail.com>
2021-04-07 08:51:09 +0900

    S3に格納しているJSONをパースし、リスト表示する処理を実装

--- app/src/main/java/com/example/awsclflearning/Fragment/LearningFragment.kt
+++ app/src/main/java/com/example/awsclflearning/Fragment/LearningFragment.kt
@@ -1,7 +1,7 @@
 package com.example.awsclflearning.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -10,9 +10,25 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.example.awsclflearning.Adapter.LearningContentAdapter
 import com.example.awsclflearning.R
+import com.github.kittinunf.fuel.core.requests.CancellableRequest
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result.Failure
+import com.github.kittinunf.result.Result.Success
 import kotlinx.android.synthetic.main.fragment_learning.*
+import org.json.JSONException
+import org.json.JSONObject
 
-class LearningFragment : Fragment() {
+class LearningFragment : BaseFragment() {
+
+    /** AWS学習コンテンツList **/
+    // 親グループの作成
+    val group: MutableList<String> = ArrayList()
+    // 子グループの作成
+    val item_group1: MutableList<String> = ArrayList()
+    val item_group2: MutableList<String> = ArrayList()
+    val item_group3: MutableList<String> = ArrayList()
+    // 集約List
+    val items: MutableList<List<String>> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,28 +37,9 @@ class LearningFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 親グループの作成
-        val group: MutableList<String> = ArrayList()
-        group.add("基礎")
-        group.add("AWSサービス")
-        group.add("オススメ書籍")
+        val httpAsync = startLearningContentsJson()
+        httpAsync.join()
 
-        // 子グループの作成
-        val item_group1: MutableList<String> = ArrayList()
-        item_group1.add("Group1ーA")
-        item_group1.add("Group2ーB")
-
-        val item_group2: MutableList<String> = ArrayList()
-        item_group2.add("Group2ーA")
-        item_group2.add("Group2ーB")
-        item_group2.add("Group2ーC")
-        item_group2.add("Group2ーD")
-
-        val item_group3: MutableList<String> = ArrayList()
-        item_group3.add("図解即戦力　Amazon Web Servicesのしくみと技術がこれ1冊でしっかりわかる教科書")
-        item_group3.add("一夜漬け AWS認定クラウドプラクティショナー 直前対策テキスト")
-
-        val items: MutableList<List<String>> = ArrayList()
         items.add(item_group1)
         items.add(item_group2)
         items.add(item_group3)
@@ -56,6 +53,9 @@ class LearningFragment : Fragment() {
             val adapter1 = parent.expandableListAdapter as LearningContentAdapter
             val groupName = adapter1.getGroup(groupPosition) as String
             val itemName = adapter1.getChild(groupPosition, childPosition) as String
+
+            showFragment(LearningDetailContentsFragment())
+
             Toast.makeText(context, "$groupName : $itemName", Toast.LENGTH_SHORT).show()
             true
         }
@@ -75,5 +75,64 @@ class LearningFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         activity?.title = getString(R.string.title_fragment_learning)
+    }
+
+    private fun startLearningContentsJson(): CancellableRequest {
+        // Listの初期化
+        clearLearningContentsList()
+
+        val async = "https://aws-clf-learning-app.s3-ap-northeast-1.amazonaws.com/sample.json".httpGet().response { request, response, result ->
+            when (result) {
+                is Success -> {
+                    try {
+                        val jsonString = String(response.data)
+                            .replace("\n","")
+                            .replace("\t","")
+                            .replace(" ","")
+                            .replace("\uFEFF","")
+                        val jsonObject = JSONObject(jsonString)
+                        var learning_groups = jsonObject.getJSONArray("aws_learning_groups")
+                        var learning_contents = jsonObject.getJSONArray("aws_learning_contents")
+
+                        repeat(learning_groups.length()) {
+                            Log.d("TEST ", "groups: ${learning_groups.getJSONObject(it).getString("title")}")
+                            group.add(learning_groups.getJSONObject(it).getString("title"))
+                        }
+
+                        repeat(learning_contents.length()) {
+                            val jsonObj = learning_contents.getJSONObject(it)
+                            when (jsonObj.getInt("id")) {
+                                1 -> {
+                                    item_group1.add(jsonObj.getString("content_name"))
+                                }
+                                2 -> {
+                                    item_group2.add(jsonObj.getString("content_name"))
+                                }
+                                3 -> {
+                                    item_group3.add(jsonObj.getString("content_name"))
+                                }
+                                else -> {
+                                    Log.d("LearningFragment", "content id not founded")
+                                }
+                            }
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+                is Failure -> {
+                    println("TEST 通信失敗")
+                }
+            }
+        }
+        return async
+    }
+
+    private fun clearLearningContentsList() {
+        group.clear()
+        item_group1.clear()
+        item_group2.clear()
+        item_group3.clear()
+        items.clear()
     }
 }
